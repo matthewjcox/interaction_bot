8a0ba635ed1d1bb0834da88f2ffe3db7a8828cad
Matthew Cox <matthewjcox.2000@gmail.com>
2020-11-28 21:22:11 -0500

    Updated with changes in the discord API about seeing member lists

--- bot.py
+++ bot.py
@@ -19,43 +19,53 @@ def get_name(member):
 interaction_role= 'wants-random-interaction'
 
 class InteractionBot(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, intents, *args, **kwargs):
+        super().__init__(intents=intents, *args, **kwargs)
         self.background_task = self.loop.create_task(self.match_loop())
 
     async def match_loop(self):
         await self.wait_until_ready()
-        #Chooses a time from 19:00 to 23:59 to match people up
+
+        #Chooses a time from 19:00 to 23:59 EDT to match people up
         next_match_hour=random.randint(19,23)
         next_match_minute=random.randint(0,59)
 
+        # Use for debugging to force a match at a time very soon
+        # next_match_hour=21
+        # next_match_minute=20
+
         while not self.is_closed():
             now = time.localtime()
-
             if next_match_hour==now.tm_hour and next_match_minute==now.tm_min:
-                print(f"it is now {now.tm_hour}:{now.tm_min}; sending matches!")
+                print(f"it is now {now.tm_hour:02d}:{now.tm_min:02d}; sending matches!")
                 await self.match_people()
                 next_match_hour = random.randint(19, 23)
                 next_match_minute = random.randint(0, 59)
-                # await asyncio.sleep(12*60*60) # Wait 12 hours
+                print(f'Will send next matches tomorrow at {next_match_hour:02d}:{next_match_minute:02d}')
+                await asyncio.sleep(12*60*60) # Wait 12 hours
+
                 # Note that new matches will be sent at the first instance of the next match time.
                 # e.g. if the new notification time is later than the current time, the new matches
                 # will be sent on the same day; if the new time is before the current time, the new
                 # matches will be sent on the next day.
             else:
-                print(f"it is now {now.tm_hour}:{now.tm_min}; waiting until {next_match_hour}:{next_match_minute}")
+                print(f"it is now {now.tm_hour:02d}:{now.tm_min:02d}; waiting until {next_match_hour:02d}:{next_match_minute:02d}")
 
             await asyncio.sleep(30) # check every 30 seconds
 
 
     async def match_people(self):
+        # print('Matching!')
         for guild in client.guilds:
             if guild.name == GUILD:
                 break
+        else:
+            return
 
         members_to_contact=[member for member in guild.members if interaction_role in [i.name for i in member.roles]]
         random.shuffle(members_to_contact)
         groups_who_bumped_into_eachother=[]
+
 
         #Figure out the groups who bumped into each other:
         while members_to_contact:
@@ -66,6 +76,7 @@ class InteractionBot(discord.Client):
                 break
             groups_who_bumped_into_eachother.append((members_to_contact[0],members_to_contact[1]))
             members_to_contact=members_to_contact[2:]
+        # print('Matches made!')
 
         #Message each person about who else they bumped into:
         for group in groups_who_bumped_into_eachother:
@@ -74,7 +85,7 @@ class InteractionBot(discord.Client):
                 try:
                     await member.create_dm()
                     if (len(group) == 2):
-                        people_bumped_into=group_minus_member[0]
+                        people_bumped_into=str(group_minus_member[0])
                     elif (len(group) == 3):
                         people_bumped_into=f"{group_minus_member[0]} and {group_minus_member[1]}"
                     else:
@@ -84,6 +95,10 @@ class InteractionBot(discord.Client):
                     await member.dm_channel.send(message)
                 except:
                     pass
+        # print('Match messages sent!')
 
-client = InteractionBot()
+
+intents = discord.Intents.default()
+intents.members = True
+client = InteractionBot(intents)
 client.run(TOKEN)
